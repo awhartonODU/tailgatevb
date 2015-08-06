a154086b0f947c21cddc60282ffab90487083900
Andrew Wharton <awharton5@gmail.com>
2015-08-05 22:52:47 -0400

    First push. Many changes.

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index.html');
+});
+
+// set the contact page route
+app.get('/contact', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('contact.html');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
