0bff0219231d5dfe8bbbdcd66ac37032636bd894
Andrew Wharton <awharton5@gmail.com>
2015-08-10 23:04:33 -0400

    fix favicon, remove event link

--- server.js
+++ server.js
@@ -33,13 +33,6 @@ app.get('/menu', function(req, res) {
     res.render('menu.html');
 });
 
-// set the menu page route
-app.post('/contact.php', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('contact.php');
-});
-
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
