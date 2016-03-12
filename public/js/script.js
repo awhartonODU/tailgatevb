9f6901cc82f376eed6c9ea689a092b7b5ccbbbe8
Andrew Wharton <awharton5@gmail.com>
2016-03-12 18:39:48 -0500

    fix flickr widget, make h2 a link

--- public/js/script.js
+++ public/js/script.js
@@ -132,7 +132,6 @@ jQuery(document).ready(function($) {
 			var numPics = data.items.length;
 			// Reset our counter to 0
 			var iCount = 1;
-			console.log('data:', data);
 
 			// Start putting together the HTML string
 			var htmlString = "<ul>";
@@ -140,7 +139,7 @@ jQuery(document).ready(function($) {
 			// Now start cycling through our array of Flickr photo details
 			$.each(data.items, function(i,item){
 				// Let's only display 6 photos (a 2x3 grid), starting from a the first point in the feed
-				if (iCount > numPics - 7 && iCount < numPics) {
+				if (iCount < 7) {
 					// I only want the ickle square thumbnails
 					var sourceSquare = (item.media.m).replace("_m.jpg", "_s.jpg");
 
